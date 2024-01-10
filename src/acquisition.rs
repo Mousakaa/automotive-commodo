1a47f0819d3a80f060b5371665ca4bd3b7cf627d
Mousakaa <arthur.gaudard@gmx.fr>
2024-01-10 17:10:36 +0100

    Last acquisition fixes

--- src/acquisition.rs
+++ src/acquisition.rs
@@ -10,14 +10,14 @@ use stm32f4xx_hal as hal;
 
 use crate::hal::{pac, gpio, prelude::*};
 
-use core::cell::Cell;
+use core::cell::RefCell;
 
-static LB_SWITCH: Mutex<Cell<Option<gpio::PC10<gpio::Input>>>> = Mutex::new(Cell::new(None));
-static RB_SWITCH: Mutex<Cell<Option<gpio::PC11<gpio::Input>>>> = Mutex::new(Cell::new(None));
-static ON_SWITCH: Mutex<Cell<Option<gpio::PC12<gpio::Input>>>> = Mutex::new(Cell::new(None));
-static HIGH_SWITCH: Mutex<Cell<Option<gpio::PC13<gpio::Input>>>> = Mutex::new(Cell::new(None));
-static AUTO_SWITCH: Mutex<Cell<Option<gpio::PC14<gpio::Input>>>> = Mutex::new(Cell::new(None));
-static LIGHT_SENSOR: Mutex<Cell<Option<gpio::PC15<gpio::Input>>>> = Mutex::new(Cell::new(None));
+static LB_SWITCH: Mutex<RefCell<Option<gpio::PC10<gpio::Input>>>> = Mutex::new(RefCell::new(None));
+static RB_SWITCH: Mutex<RefCell<Option<gpio::PC11<gpio::Input>>>> = Mutex::new(RefCell::new(None));
+static ON_SWITCH: Mutex<RefCell<Option<gpio::PC12<gpio::Input>>>> = Mutex::new(RefCell::new(None));
+static HIGH_SWITCH: Mutex<RefCell<Option<gpio::PC13<gpio::Input>>>> = Mutex::new(RefCell::new(None));
+static AUTO_SWITCH: Mutex<RefCell<Option<gpio::PC14<gpio::Input>>>> = Mutex::new(RefCell::new(None));
+static LIGHT_SENSOR: Mutex<RefCell<Option<gpio::PC15<gpio::Input>>>> = Mutex::new(RefCell::new(None));
 
 pub fn init(dp_gpioc: pac::GPIOC, dp_syscfg: pac::SYSCFG, exti: &mut pac::EXTI) {
     let gpioc = dp_gpioc.split();
@@ -81,40 +81,37 @@ pub fn init(dp_gpioc: pac::GPIOC, dp_syscfg: pac::SYSCFG, exti: &mut pac::EXTI) 
 
 
 pub fn send_byte(cs: &CriticalSection) -> u8 {
-    let left_blinker = LB_SWITCH.borrow(cs).take()
+    let left_blinker_mut = LB_SWITCH.borrow(cs).borrow();
+    let left_blinker = left_blinker_mut.as_ref()
         .expect("Left blinker uninitialized");
 
-    let right_blinker = RB_SWITCH.borrow(cs).take()
+    let right_blinker_mut = RB_SWITCH.borrow(cs).borrow();
+    let right_blinker = right_blinker_mut.as_ref()
         .expect("Right blinker uninitialized");
 
-    let on_switch = ON_SWITCH.borrow(cs).take()
+    let on_switch_mut = ON_SWITCH.borrow(cs).borrow();
+    let on_switch = on_switch_mut.as_ref()
         .expect("Lights ON switch uninitialized");
 
-    let high_switch = HIGH_SWITCH.borrow(cs).take()
+    let high_switch_mut = HIGH_SWITCH.borrow(cs).borrow();
+    let high_switch = high_switch_mut.as_ref()
         .expect("High beam switch uninitialized");
 
-    let auto_switch = AUTO_SWITCH.borrow(cs).take()
+    let auto_switch_mut = AUTO_SWITCH.borrow(cs).borrow();
+    let auto_switch = auto_switch_mut.as_ref()
         .expect("Auto mode switch uninitialized");
 
-    let light_sensor = LIGHT_SENSOR.borrow(cs).take()
+    let light_sensor_mut = LIGHT_SENSOR.borrow(cs).borrow();
+    let light_sensor = light_sensor_mut.as_ref()
         .expect("Light sensor uninitialized");
 
     let byte = 0u8
-        | left_blinker.is_high() as u8              // Left blinker value
-        | ((right_blinker.is_high() as u8) << 1)   // Right blinker value
-        | if auto_switch.is_high() {                // Lights ON/OFF
-              light_sensor.is_low() as u8
-          } else {
-              on_switch.is_high() as u8
-          } << 2
-        | ((high_switch.is_high() as u8) << 3);     // High or low beam lights
-
-    LB_SWITCH.borrow(cs).set(Some(left_blinker));
-    RB_SWITCH.borrow(cs).set(Some(right_blinker));
-    ON_SWITCH.borrow(cs).set(Some(on_switch));
-    HIGH_SWITCH.borrow(cs).set(Some(high_switch));
-    AUTO_SWITCH.borrow(cs).set(Some(auto_switch));
-    LIGHT_SENSOR.borrow(cs).set(Some(light_sensor));
+        | left_blinker.is_high() as u8                              // Left blinker value
+        | ((right_blinker.is_high() as u8) << 1)                    // Right blinker value
+        | (on_switch.is_high()                                      // Switch lights on or off
+            || (auto_switch.is_high() && light_sensor.is_low())     // Even in auto mode
+          as u8 ) << 2
+        | ((high_switch.is_high() as u8) << 3);                     // High or low beam lights
 
     return byte;
 }
